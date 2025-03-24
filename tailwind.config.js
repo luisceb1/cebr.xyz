9c73fe5c420a666e47ba76436339eb7dd8c310c1
luisceb1 <55941735+luisceb1@users.noreply.github.com>
2025-03-24 18:47:28 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/index.js",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
